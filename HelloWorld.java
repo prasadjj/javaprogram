722f8c1235f9811f8746bfae9df514db20473e26
Prasad J <116068753+prasadjj@users.noreply.github.com>
2022-10-18 14:47:13 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	   System.out.println("Hello world");
+	   System.out.println("I'm Prasad");
+	   System.out.println("I'm From Chennai");
+	   System.out.println("I am study in edubridge");
+	   System.out.println("java full stack");
+	   }
+
+}
