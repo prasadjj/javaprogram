e05589671af3c6114422606bddcd72aff1772b6a
Prasad J <116068753+prasadjj@users.noreply.github.com>
2022-12-02 15:18:55 +0530

    Add files via upload

---
+++ Fibnocci.java
@@ -0,0 +1,28 @@
+package com.edu;
+
+import java.util.Scanner;
+
+public class Fibnocci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t,f1=1,f2=1,f3,n=1;
+
+		Scanner ps=new Scanner(System.in);
+		System.out.println("enter the number ");
+		t=ps.nextInt();
+		System.out.println("Fibonic Series");
+		System.out.println(f1);
+		System.out.println(f2);
+
+		while(n<=t-2)
+		{
+			f3=f1+f2;
+			System.out.println(f3);
+			f1=f2;
+			f2=f3;
+			n=n+1;
+		}
+	}
+
+}
