e45c964066a59cccd41a96a6fdc240f2719d72f1
Prasad J <116068753+prasadjj@users.noreply.github.com>
2022-12-03 03:07:25 +0530

    Add files via upload

---
+++ Mainshape.java
@@ -0,0 +1,43 @@
+package com.edu;
+
+abstract class Shape{
+	abstract void area();
+}
+
+class rectangle extends Shape{
+
+	void area() {
+		int a,b,c;
+		a=5;
+		b=4;
+		c=a*b;
+		System.out.println("rectangule area of: "+c);
+	}
+}
+
+class Square extends Shape{
+	void area() {
+		int a,b;
+		b=5;
+		a=b*b;
+		System.out.println("square area of: "+a);
+	}
+}
+
+public class Mainshape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rectangle ps=new rectangle();
+		ps.area();
+		Square ns=new Square();
+		ns.area();
+		//
+		Shape obj;
+		obj=new rectangle();
+		obj.area();
+		obj=new Square();
+		obj.area();
+	}
+
+}
