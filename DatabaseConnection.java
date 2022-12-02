28d96d24d90ab70681bf1c29cbb41af1464219c7
Prasad J <116068753+prasadjj@users.noreply.github.com>
2022-12-03 03:21:25 +0530

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,29 @@
+package Com.edu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	private static String driver="com.mysql.cj.jdbc.Driver";
+	private static String un="root";
+	private static String up="root";
+	private static String url="jdbc:mysql://localhost:3306/afternoon_batch";
+	private static Connection conn=null;
+	public static Connection getConnection() {
+		
+		try {
+			Class.forName(driver);
+			conn=DriverManager.getConnection(url,un,up);
+			if(conn==null) {
+			
+				System.out.println("Error in connection");
+				System.exit(0);;
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
